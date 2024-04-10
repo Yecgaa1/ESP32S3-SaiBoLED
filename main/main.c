97de7fcd8f34afccf05192943d3e772362b14c74
xtx <2656823393@qq.com>
2024-04-10 14:59:02 +0800

    添加了开灯测试代码

--- main/main.c
+++ main/main.c
@@ -9,6 +9,7 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
+#include <esp_log.h>
 #include <stdio.h>
 #include <inttypes.h>
 #include <soc/clk_tree_defs.h>
@@ -22,12 +23,14 @@
 
 #define BLINK_GPIO_UP 5
 #define BLINK_GPIO_DOWN 6
+#define TAG "app_main"
+#define LED_STRIP_LED_NUMBERS 63
 
 void app_main(void) {
 	//初始化上层灯
 	led_strip_config_t strip_config_up = {
 		.strip_gpio_num = BLINK_GPIO_UP, // The GPIO that connected to the LED strip's data line
-		.max_leds = 1, // The number of LEDs in the strip,
+		.max_leds = 63, // The number of LEDs in the strip,
 		.led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
 		.led_model = LED_MODEL_WS2812, // LED strip model
 		.flags.invert_out = false,
@@ -48,7 +51,7 @@ void app_main(void) {
 	//初始化下层灯
 	led_strip_config_t strip_config_down = {
 		.strip_gpio_num = BLINK_GPIO_DOWN, // The GPIO that connected to the LED strip's data line
-		.max_leds = 1, // The number of LEDs in the strip,
+		.max_leds = 63, // The number of LEDs in the strip,
 		.led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
 		.led_model = LED_MODEL_WS2812, // LED strip model
 		.flags.invert_out = false,
@@ -66,5 +69,24 @@ void app_main(void) {
 	};
 	ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config_down, &rmt_config_down, &led_strip_down));
 
+	ESP_LOGI(TAG, "Start blinking LED strip");
+	bool led_on_off= true;
+	while (1) {
+		if (led_on_off) {
+			/* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+			for (int i = 0; i < LED_STRIP_LED_NUMBERS; i++) {
+				ESP_ERROR_CHECK(led_strip_set_pixel(led_strip_down, i, 5, 5, 5));
+			}
+			/* Refresh the strip to send data */
+			ESP_ERROR_CHECK(led_strip_refresh(led_strip_down));
+			ESP_LOGI(TAG, "LED ON!");
+		} else {
+			/* Set all LED off to clear all pixels */
+			ESP_ERROR_CHECK(led_strip_clear(led_strip_down));
+			ESP_LOGI(TAG, "LED OFF!");
+		}
 
+		led_on_off = !led_on_off;
+		vTaskDelay(pdMS_TO_TICKS(1500));
+	}
 }
